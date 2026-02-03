42189f77e973bdd830aef2d6f0d3dd8f5614a42c
mcsurvivalmode <C24385051@mytudublin.ie>
2026-02-03 00:33:32 +0000

    sound effects working yaya

---
+++ ASSIGNMENT/Programming_Assignment/Assets/Script/behaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class behaviour : StateMachineBehaviour {
+
+	// Create AudioSource array to hold all AudioSource Comnponents
+	private AudioSource[] audio;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+		// Get access to GetComponentsInParent, this will return an array
+		audio = animator.GetComponentsInParent<AudioSource> ();
+		// PLay audio file at index 0 (Door open)
+		audio [0].Play ();
+	}
+
+    	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+
+		// Get access to GetComponentsInParent, this will return an array
+		audio = animator.GetComponentsInParent<AudioSource> ();
+		// PLay audio file at index 0 (Door open)
+		audio [0].Stop ();
+	}
+
+}
