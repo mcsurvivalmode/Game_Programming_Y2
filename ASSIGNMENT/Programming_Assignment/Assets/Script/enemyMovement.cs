5db4cfb2747552df7b8d385fb5ff7f3e9683ecb2
mcsurvivalmode <C24385051@mytudublin.ie>
2026-02-23 21:33:30 +0000

    collison stuff

--- ASSIGNMENT/Programming_Assignment/Assets/Script/enemyMovement.cs
+++ ASSIGNMENT/Programming_Assignment/Assets/Script/enemyMovement.cs
@@ -5,118 +5,86 @@ using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
-    // Reference to NavMeshAgent component
     NavMeshAgent navAgent;
     Vector3 destination;
 
-    // Canvas Array
+    //canvas update on state!
     public Text[] canvasUpdate;
 
-    // Decision Tree control booleans 
     public bool isVisible;
     public bool isAudible;
     public bool isClose;
 
-    // Also to the game object we will follow
+    //so enemy follows the player
     public Transform targetObject;
-
-    //Controlling Navmesh and Animation
+    Animator anim; 
+   
     Vector3 worldDeltaPosition;
     Vector2 groundDeltaPosition;
     Vector2 velocity = Vector2.zero;
 
-    // Animator Object
-    Animator anim;
+    
 
-    // Waypoints for Patrol functionality
+  
     int nextIndex;
-    public GameObject[] waypoints;
+    public GameObject[] waypoints; //so the enemy will patrol likek guards till player is near 
 
-    // Sight variables
-    public float fieldOfViewAngle = 360.0f;
+    
+    public float fieldOfViewAngle = 360.0f; //what the enemy can see 
     private SphereCollider col;
 
 
     void Start()
     {
-        // Get Animator
+        
         anim = GetComponent<Animator>();
-        // Get NavAgent
         navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         navAgent.Warp(transform.position);
-
-        // Stop agent navigating automatically by setting the agents updatePosition property to false.
-        // This means that the position of the NavMesgAgent component is decoupled from the position of
-        // the character, i.e the 2 positions can move independently. In each videoframe, the agent
-        // calculates its next position but does not automatically move the character there.
         navAgent.updatePosition = false;
 
-        // Patrol Scene
         destination = NextWaypoint(Vector3.zero);
-
         col = GetComponent<SphereCollider>();
     }
 
-    // This is where we can create our Decision Tree
-    void Update()
+    
+    void Update() //decision tree 
     {
-        // Check if target object exists
-        // If so, runn Perception Tests
+
         if (targetObject)
         {
-            // Check is the Player is Visible
             isPlayerVisible();
-            // Check is the Player is Audible
             isPlayerAudible();
-            // Check is the Player is Visible
             isPlayerClose();
 
-
-            // Check for visibility and proximity
             if (isVisible && isClose)
             {
-                // If Randomball is visible and is close, then SEEK
-                seekFunction();
+                seekFunction(); //is visible and near 
             }
             else if (isVisible && !isClose)
             {
-                // If Randomball is visible and not close, then PATROL
-                patrolFunction();
+                patrolFunction(); //is visible and not near 
             }
             else if (!isVisible && !isAudible)
             {
-                // If Randomball is not visible and not audable, then PATROL
-                IdleFunction();
+                IdleFunction(); //isnt visible or close 
             }
             else if (!isVisible && isAudible)
             {
-                // If Randomball is visible and not close, then PATROL
                 patrolFunction();
             }
         }
         else
         {
-            // If RandomBall has been destroyed then Idle
             IdleFunction();
         }
 
         navAgent.SetDestination(destination);
-
-        // Now we determine what the NavMesh Agents next move would be but dont move.
-        // Instead we ask the agent what its next position would be and compare that
-        // to our current position to create a worldDeltaPosition which is how
-        // the AI agent thinks the character should move in 3D.
         worldDeltaPosition = navAgent.nextPosition - transform.position;
-        // We then take the Dot Products to obtain the components in the forward directions
-        // and sideways direction and call that groundDeltaPosition.
         groundDeltaPosition.x = Vector3.Dot(transform.right, worldDeltaPosition);
         groundDeltaPosition.y = Vector3.Dot(transform.forward, worldDeltaPosition);
-        // We divide that by the time duration of the frame to get the velocity the character should move
-        // and if that is greater than a small number, i.e 1 to the power -5 = 0.00001 (C# Ternary Operator Usage)
         velocity = (Time.deltaTime > 1e-5f) ? groundDeltaPosition / Time.deltaTime : velocity = Vector2.zero;
-        // And we have not yet arrived at the destination....,
         bool shouldMove = velocity.magnitude > 0.025f && navAgent.remainingDistance > navAgent.radius;
-        // We set the move parameter to true and set the velx and vely animator parameters. 
+
         anim.SetBool("move", shouldMove);
         anim.SetFloat("velx", velocity.x);
         anim.SetFloat("vely", velocity.y);
@@ -126,86 +94,64 @@ public class enemyMovement : MonoBehaviour
 
     void OnAnimatorMove()
     {
-
         transform.position = navAgent.nextPosition;
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "RandomBall")
+        if (col.gameObject.name == "player")
         {
-            // Stop the chase by destroying the yellow ball
-            //Destroy (col.gameObject);
-
-            //         // Update all Canvas Items with Dead!
-            //         for (int i = 0; i < canvasUpdate.Length; i++)
-            //         {
-            //             canvasUpdate[i].text = "Dead!";
-            //         }
+            canvasUpdate[6].text = "Player: Caught";
 
         }
-        else if (col.gameObject.name == "Cube")
+        else if (col.gameObject.name == "Cube") //changing coloyr of stuff when touched   
         {
-            // Change the colour of the object
             col.gameObject.GetComponent<Renderer>().material.color = Color.green;
         }
     }
-    // Seek out RandomBall
+   
     void seekFunction()
     {
-        // Seek out the Enemy
         destination = targetObject.position;
-        // Update Canvas with current state
         canvasUpdate[3].text = "Seeking";
     }
-    // Patrol Array of cubes (added to script)
     void patrolFunction()
     {
-        // If within 2.5, then move onto next waypoint in array
         if (Vector3.Distance(transform.position, destination) < 2.5)
         {
             destination = NextWaypoint(destination);
         }
-        // Update Canvas with current state
         canvasUpdate[3].text = "Patrolling";
     }
-    // Idle at (0,0,0)
     void IdleFunction()
     {
-        // Idle at 0
-        destination = new Vector3(0, 0, 0);
-        // Update Canvas with current state
+        destination = NextWaypoint(destination);
         canvasUpdate[3].text = "Idling";
     }
 
-    // Function that loops through waypoints for the Patrol fucntionality
-    public Vector3 NextWaypoint(Vector3 currentPosition)
+    
+    public Vector3 NextWaypoint(Vector3 currentPosition) //patroling shiz
     {
         Debug.Log(currentPosition);
         if (currentPosition != Vector3.zero)
         {
-            // Find array index of given waypoint
             for (int i = 0; i < waypoints.Length; i++)
             {
-                // Once found calculate next one
                 if (currentPosition == waypoints[i].transform.position)
                 {
-                    // Modulus operator helps to avoid to go out of bounds
-                    // And resets to 0 the index count once we reach the end of the array
                     nextIndex = (i + 1) % waypoints.Length;
                 }
             }
         }
         else
         {
-            // Default is first index in array 
             nextIndex = 0;
         }
         return waypoints[nextIndex].transform.position;
     }
 
-    // Checks if Player is visible using Raycast
-    public void isPlayerVisible()
+    
+    public void isPlayerVisible() //VISIBLE STATE (RAYCAST)
     {
 
         // Create a vector from the enemy to the player and store the angle between it and forward.
@@ -237,8 +183,8 @@ public class enemyMovement : MonoBehaviour
         }
     }
 
-    // Checks if player is Audible using simple distance calculation
-    public void isPlayerAudible()
+    
+    public void isPlayerAudible() //AUDIO STATE
     {
         // If direct distance < 20, then audible
         if (Vector3.Distance(transform.position, targetObject.position) < 20.0f)
@@ -257,36 +203,25 @@ public class enemyMovement : MonoBehaviour
         }
     }
 
-    // Check is Player is CloseBy based on the NavMesh
-    // Distance to Player via the NavMesh is calculated here
-    // If Distance < 30, then isClose == true
-    public void isPlayerClose()
+    public void isPlayerClose() //PLAYER DISTANCE STATE
     {
-
-        // Create a path and set it based on a target position.
         NavMeshPath path = new NavMeshPath();
         if (navAgent.enabled && navAgent.isOnNavMesh)
             navAgent.CalculatePath(targetObject.position, path);
 
-        // Create an array of points which is the length of the number of corners in the path + 2.
+        
         Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
-
-        // The first point is the enemy's position.
         allWayPoints[0] = transform.position;
 
-        // The last point is the target position.
         allWayPoints[allWayPoints.Length - 1] = targetObject.position;
-
-        // The points inbetween are the corners of the path.
         for (int i = 0; i < path.corners.Length; i++)
         {
             allWayPoints[i + 1] = path.corners[i];
         }
 
-        // Create a float to store the path length that is by default 0.
         float pathLength = 0;
 
-        // Increment the path length by an amount equal to the distance between each waypoint and the next.
+      
         for (int i = 0; i < allWayPoints.Length - 1; i++)
         {
             pathLength += Vector3.Distance(allWayPoints[i], allWayPoints[i + 1]);
