7ec8d99313de9e49dc45ba593b552274a996688b
Ava-TU <C24385051@mytudublin.ie>
2026-04-30 17:34:30 +0100

    tried to make camera work but sigh it didnt whateverv

---
+++ ASSIGNMENT/Programming_Assignment/Assets/NETCODE - client authorized/CameraFollow.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+using Unity.Netcode;
+
+public class CameraFollow : NetworkBehaviour
+{
+    private GameObject m_camera;
+    Vector3 m_offsetFromPlayer;
+    Vector3 m_originPosition;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if(IsOwner == false )
+        {
+            return;
+        }
+        m_camera = GameObject.FindWithTag("Main Camera");
+        m_originPosition = m_camera.transform.position;
+        m_offsetFromPlayer= transform.position - m_camera.transform.position;  
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if(IsOwner && m_camera != null )
+        {
+            m_camera.transform.position = m_offsetFromPlayer;
+        }
+        base.OnNetworkDespawn();
+    }
+
+    private void LateUpdate()
+    {
+        if (IsOwner && m_camera != null)
+        {
+            m_camera.transform.position = transform.position - m_offsetFromPlayer;
+        }
+
+    }
+}
